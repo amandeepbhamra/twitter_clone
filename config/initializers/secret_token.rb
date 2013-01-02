d1aa16bd0041b2853c7faea392b3b67b1ea56093
Amandeep <fizzy@sahil-Vostro-1014.(none)>
2013-01-02 12:42:24 +0530

    devise and bootstrap done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '54ee536cd10ebed560baeeee6fe74f89f5ff2f342d9878f7bf4b3f3b8d0d5880d62c3f9d513cf7bac6c9ed5818940bfc790a63ae2975484bac5d691e3cca1454'
---
+++ db/migrate/20121231132910_add_users_id_to_tweets.rb
@@ -0,0 +1,8 @@
+class AddUsersIdToTweets < ActiveRecord::Migration
+  def self.up
+  	add_column :tweets, :user_id, :integer
+  end
+  def self.down
+  	remove_column :tweets, :user_id
+  end
+end
