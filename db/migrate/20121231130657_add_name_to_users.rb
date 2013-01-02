960d3b67c3c8891a2587799215c7a775dc47a3de
Amandeep <fizzy@sahil-Vostro-1014.(none)>
2013-01-02 15:25:55 +0530

    deploying on heroku

--- db/migrate/20121231130657_add_name_to_users.rb
+++ db/migrate/20121231130657_add_name_to_users.rb
@@ -1,22 +1,14 @@
 class AddNameToUsers < ActiveRecord::Migration
   def self.up
   	add_column :users, :name, :string
-  	add_column :users, :password, :string
-  	add_column :users, :email, :string
-	add_column :users, :mobile, :integerrake 
+  	add_column :users, :mobile, :integer 
   	add_column :users, :location, :string
   	add_column :users, :bio, :text
-  	add_column :users, :photo, :string
-  	add_column :users, :headerphoto, :string
   end
   def self.down
   	remove_column :users, :name
-	remove_column :users, :password
-	remove_column :users, :email
 	remove_column :users, :mobile
 	remove_column :users, :location
 	remove_column :users, :bio
-	remove_column :users, :photo
-	remove_column :users, :headerphoto
-  end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231172127) do
+ActiveRecord::Schema.define(:version => 20121231132910) do
 
   create_table "tweets", :force => true do |t|
     t.text     "status"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(:version => 20121231172127) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.integer  "mobile"
+    t.string   "location"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
