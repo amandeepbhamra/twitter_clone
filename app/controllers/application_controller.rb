407af48083c70007da11f33a3837c81b45ef7eb5
Amandeep <amandeep.bhamra@gmail.com>
2013-01-04 10:40:35 +0530

    following done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def after_sign_up_path_for(current_user)
-    redirect_to user_path
+ def after_sign_in_path_for(resource)
+    #stored_location_for(resource) || 
+  user_path(resource)
   end
    
 end
