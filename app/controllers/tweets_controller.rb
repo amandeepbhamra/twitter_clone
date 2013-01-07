c3a65d088b0b3eb26a86865e5c2ac98c1c7ed5b4
Amandeep <amandeep.bhamra@gmail.com>
2013-01-07 12:48:10 +0530

    actions removed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,7 @@ class TweetsController < ApplicationController
   
   def index
 
-    @tweets = @user.tweets
+    @tweets = @user.tweets.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
