ae19504580c381ef4fbccd4099b5bfd321fbd5f3
Amandeep <amandeep.bhamra@gmail.com>
2013-01-09 13:39:38 +0530

    added geocoder functionality

---
+++ db/migrate/20130109071641_add_longitude_to_users.rb
@@ -0,0 +1,10 @@
+class AddLongitudeToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+  end
+  def self.down
+    remove_column :users, :latitude
+    remove_column :users, :longitude
+  end
+end
