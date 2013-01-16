26c2eb62bdaf106ea79e6221f25f907c19698a32
Nitin Grover <niting@fizzysoftware.com>
2013-01-16 16:30:51 +0530

    tweet animation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,8 @@
 jQuery(function(){
   $("textarea.expand").click(function () {
-    $(this).animate({ "height" : "100px" }, 500);
+    $(this).animate({ "height" : "100px" }, 200);
+	});
+  $(".tweet_btn").click(function () {
+    $("textarea.expand").animate({ "height" : "40px" }, 200);
 	});
 });
