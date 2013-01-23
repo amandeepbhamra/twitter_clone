34c32097835eaac2abc498c20965d1003953d728
Nitin Grover <niting@fizzysoftware.com>
2013-01-23 15:41:31 +0530

    jquery plugin

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,4 +5,10 @@ $(function(){
   $(".tweet_btn").click(function () {
     $("textarea.expand").animate({ "height" : "40px" }, 200);
 	});
+alert("this is alert");
+  $("body").ezBgResize({
+            img : "/images/bg.jpg", // Required.
+        });
 });
+
+		
