1630ba73cf5872613d9f3a90874b0fdfc2aa906b
Nitin Grover <niting@fizzysoftware.com>
2013-02-01 14:01:12 +0530

    jquery remove

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,4 @@
 $(function(){
-  $("textarea.expand").click(function () {
-    $(this).animate({ "height" : "100px" }, 200);
-	});
-  $(".tweet_btn").click(function () {
-    $("textarea.expand").animate({ "height" : "40px" }, 200);
-	});
-
   $("body").ezBgResize({
             img : "/images/bg.jpg", // Required.
         });
