702aef70da897ce2296a230e2a16b01b5fdd7f4b
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-14 11:07:54 +0530

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,9 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update_attributes(params[:user])
+        # To notfiy user that profile is updated, a mail is sent after 2 mins #
         Notify.delay(queue: "update email notification", priority: 3, run_at: 2.minutes.from_now ).update_notification(@user)
+        
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
